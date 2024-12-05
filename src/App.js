61a06f4d86ff025b2c862971107c0211c0564b69
unizazang <zifnffk32123@gmail.com>
2024-12-06 00:58:57 +0900

    프로젝트 생성

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+
+  let post = '강남 우동 맛집';
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div >
+        <h4 className="black-nav" >블로그임</h4>
+      </div>
+      <h4>{post}</h4>
+      
+      {/* 변수는 중괄호에 넣으면 됨
+      id 자리에/className자리에 이런데도 다 들어감.
+      */}
     </div>
   );
 }
