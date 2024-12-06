450926943d0e86da7db4d36c1ec1c38491087d0f
unizazang <zifnffk32123@gmail.com>
2024-12-07 00:55:33 +0900

    블로그 제목

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-
-  let post = '강남 우동 맛집';
-  return (
-    <div className="App">
-      <div >
-        <h4 className="black-nav" >블로그임</h4>
-      </div>
-      <h4 style={{color:'red'}}>{post}</h4>
-      
-    <div className='list'>
-      <h4>글제목</h4>
-      <p>12월 6일 발행</p>
-    </div>
 
 
       {/* 
@@ -41,6 +29,59 @@ function App() {
       fontSize (O)
 
       */}
+
+      
+
+  let post = '역삼역 우동 맛집'; 
+  //자료를 잠깐 저장하는 건 변수, 근데 리액트에선 자료를 잠깐 저장할 때 state라는 걸 써도 된다.
+//useState('남자 코트 추천');
+//useState 라고 하면 스테이트를 만들 수 있다
+//내가 넣고 싶은 자료를 소괄호 안에 저장해 두는 것
+
+let [글제목, b] = useState('여자 코트 추천'); // 이 state를 갖다 쓰고싶으면 왼쪽에다가 자료를 뽑는 문법을 짜면 됨.
+// a = state에 보관했던 자료
+// b = state를 변경할 수 있게 해주는 함수
+// 각각 a랑 b로 빼서 쓰겠습니다~~     저 useState 안에는 [ ?? , ???] 이렇게 들어있음 그거를 각각 빼왔을 뿐임
+//destructuring 이라는 문법입니다 array 안에 있는 자료들을 각각 변수로 빼주는 문법이다
+//let num = [1,2,3];
+//let [a, c] = [1, 2];
+      //let a = num[0]; 
+      //let c = num[1];
+
+      //왜 state를 쓰느냐? 일반 변수는 갑자기 내용이 변경되면 HTML에 자동으로 반영안됨. (post가 바뀌었으니까 html에서도 변경을 해 줘라 라고 코드를 짜야함)
+      // 근데 state는 갑자기 변경되면 state를 쓰던 html이 알아서 자동으로 재 랜더링이 된다.
+      // state가 변경이 된다 > 그럼 이 state를 쓰는 html 전체가 재랜더링> 반영이 된다.
+      // 온갖 거 다 state로 만들지 말고 변경될때마다 html에 반영되어야 하는 것만 state로 만들어라.
+
+
+      // 빡대가리식 정리 ----------
+      // 자주 변경될 거 같은 html 부분들은 state로 만들어 놓는다.
+
+  let [글제목2, c] = useState('강남 우동 맛집');
+  let [글제목3, d] = useState('남성 헤어샵 추천');
+
+  return (
+    // 주의할 점 : HTML은 리턴 소괄호 안에 다 짜야하는데 리턴 소괄호 안에는 무조건 하나의 태그로 시작해서 하나의 태그로 끝나야 한다.
+    // 단 하나의 태그로 묶여야 한다
+
+    <div className="App">
+      <div >
+        <h4 className="black-nav" >ReactBlog</h4>
+      </div>
+      <h4 style={{color:'red'}}>{post}</h4>
+       
+    <div className='list'>
+      <h4>{ 글제목 }</h4>
+      <p>12월 6일 발행</p>
+    </div>
+    <div className='list'>
+      <h4>{ 글제목2 }</h4>
+      <p>12월 6일 발행</p>
+    </div>
+    <div className='list'>
+      <h4>{ 글제목3 }</h4>
+      <p>12월 6일 발행</p>
+    </div>
     </div>
   );
 }
